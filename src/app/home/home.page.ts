77a451d10be13c7d011308e4695b2ffc80d8a960
Francisco Soto <soto.dominguez.jf@gmail.com>
2022-03-26 21:08:03 -0600

    Test finalizado de la empresa AriGroup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  mensaje: string;
+  mostrarError: boolean = false;
+
+  constructor(public alertController: AlertController) {
+    // console.log(this.mensaje);
+  }
+  async enviarMensaje() {
+    if(this.mensaje == null || this.mensaje == undefined || this.mensaje == "" ) {
+      this.mostrarError = true;
+    } else {
+      this.mostrarError = false;
+      const alert = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        header: 'Bienvenido',
+        message: this.mensaje,
+        buttons: ['OK']
+      });
+  
+      await alert.present();
+  
+      const { role } = await alert.onDidDismiss();
+      console.log('onDidDismiss resolved with role', role);
+
+      this.mensaje = '';
+    }
+  }
 
 }
